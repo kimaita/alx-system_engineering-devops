7c50225e35e450573a28c79bb4432e03a65c1c36
kimaita <johnnieqaym@gmail.com>
2024-03-29 01:36:46 +0300

    fix for Werkzeug version incompatibility

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,14 @@
 # Installs flask (v2.1.0) from pip3.
 $pkg = 'flask'
+$pkg_dep = 'Werkzeug'
 
 package { $pkg:
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package[$pkg_dep]
+}
+
+package { $pkg_dep:
+  ensure   => '2.2.2',
+  provider =>'pip3'
 }
