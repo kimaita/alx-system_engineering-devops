0813ade8842ea43f37f4b61f51ef628887f8c3b7
kimaita <johnnieqaym@gmail.com>
2024-06-19 00:36:51 +0300

    limit instead of count

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,7 +12,7 @@ def top_ten(subreddit):
     subreddit (str): subreddit to get hot posts.
     """
 
-    url = "https://www.reddit.com/r/{}/hot.json?count=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"user-agent": "alx-api_advanced (u/kimaita)"}
     resp = requests.get(url, headers=headers, allow_redirects=False)
     try:
