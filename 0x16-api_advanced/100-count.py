623c0abfe1f1b79156328973e933ee328ffeb60f
kimaita <johnnieqaym@gmail.com>
2024-06-19 00:33:18 +0300

    processing after recursive requests

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,17 +2,18 @@
 """Contains a function that prints the frequency of given keywords in
 the titles of hot articles of a sub.
 """
+
 import requests
 
 
-def count_words(subreddit, word_list, freq=None, after=None, session=None):
+def count_words(subreddit, word_list, posts=[], after=None, session=None):
     """Queries the Reddit API for the titles of all hot posts
     for a given subreddit, then parses the title and prints keyword occurrence.
 
     Args:
         subreddit str: subreddit to get hot posts
         word_list [str]: list of keywords
-        freq {str: int}: keyword frequency dictionary
+        posts []: list of returned posts
         after str: pagination
         session requests.Session: a session for persisting connection
 
@@ -28,24 +29,22 @@ def count_words(subreddit, word_list, freq=None, after=None, session=None):
 
     if session is None:
         session = requests.Session()
-        header = {'user-agent': 'alx-api_advanced (u/kimaita)'}
+        header = {"user-agent": "alx-api_advanced (u/kimaita)"}
         session.headers.update(header)
 
     resp = session.get(endpoint, allow_redirects=False)
     try:
-        resp_data = resp.json().get('data')
-        next = resp_data.get('after')
-        posts = resp_data.get('children')
-
-        if freq is None:
-            word_list = [word.lower() for word in word_list]
-            freq = dict.fromkeys(word_list, 0)
-
-        for post in posts:
-            parse_title(post.get('data').get('title').lower(), freq)
+        resp_data = resp.json().get("data")
+        next = resp_data.get("after")
+        posts += resp_data.get("children")
 
         if next:
-            return count_words(subreddit, word_list, freq, next, session)
+            return count_words(subreddit, word_list, posts, next, session)
+
+        freq = {word.lower(): 0 for word in word_list}
+
+        for post in posts:
+            parse_title(post.get("data").get("title").lower(), freq)
 
         for w in sorted(freq.items(), reverse=True, key=lambda x: x[1]):
             if w[1]:
